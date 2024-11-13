37c64dbd871107d438370c701347034507b42396
gnefedev <gnefedev@none.com>
2024-11-13 17:42:35 +0000

    update

--- beaver/apply.sh
+++ beaver/apply.sh
@@ -3,6 +3,13 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install $packages
 
+if command -v git >/dev/null 2>&1
+then
+    echo "git is installed and in PATH"
+else
+    echo "git is not installed or not in PATH"
+fi
+
 if [[ -n $(git status -s -uall) ]]
 then
     git add *
