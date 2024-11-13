d3c0c3e7e479dcd5ec2f23ade328ced70f218c34
gnefedev <gnefedev@none.com>
2024-11-13 15:07:43 +0000

    update

---
+++ beaver/apply.sh
@@ -0,0 +1,11 @@
+packages=`cat packages.txt`
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install $packages
+
+if [[ -n $(git status -s -uall) ]]
+then
+    git add *
+    git commit -m 'update'
+    git push
+fi
